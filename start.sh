c2fd1d26b3f9e2fcffc2d405d513071ed2027dda
Sten Turpin <sten@redhat.com>
2017-05-12 20:53:43 -0500

    write files where we can write files ffs

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pushd /opt/app-root/src/riot
+cp -r /opt/app-root/src/riot /tmp
+pushd /tmp/riot
 [ -n $HOMESERVER ] && \
   cat config.sample.json | sed s#https://matrix.org#$HOMESERVER# > config.json
 
